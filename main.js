10bbf71cee93aedd15d253546f36672525ca4c35
Omar_Ismail <92218882+Mory-Smile@users.noreply.github.com>
2022-08-14 02:37:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let body = document.querySelector("body");
+let modeToggle = body.querySelector(".mode-toggle");
+let sidebar = body.querySelector("nav");
+let sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  body.classList.toggle("close");
+}
+
+// let swi = document.querySelector(".switch");
+
+// swi.addEventListener("click", () => {
+//   body.classList.toggle("active");
+// });
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
+});
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
+});
